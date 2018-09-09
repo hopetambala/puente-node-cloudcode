c38aed69458b9da0e2c6455df06ac54c464db319
ProfePosh <hpbala@umich.edu>
2018-09-09 12:26:32 -0400

    refactor(aggregate): also add retrieve class by patient ID

--- cloud/functions/aggregate.js
+++ cloud/functions/aggregate.js
@@ -1,40 +1,43 @@
-function retrieveAll(className){
-    //Creates local object based on "SurveyData" Object in Parse-Server
-    const Class = Parse.Object.extend(className);
-    
-    //Queries the SurveyData class from Parse Server
-    let query = new Parse.Query(Class);
-    
-    query.limit(2000);
-    //Below searches what's in the surveyPoints array
-    query.find().then((results) => {
-        resolve(results);
-    }, (error) => {
-        reject(error);
-    });
-}
-
-function retrieveByOrganization(className, organization){
-    //return new Promise((resolve,reject)=> {
-    var PatientDemographics = Parse.Object.extend(className);
-    var q = new Parse.Query(PatientDemographics);
-    q.limit(2000);
-    q.equalTo("surveyingOrganization", organization);
-    q.find().then((results) =>{
-        resolve(results);
-    }, (error) => {
-        reject(error);
-    });
-    //});
-}
-
-function retrieveByPatient(className,patientID){
-
-}
+'use strict';
 
 module.exports = {
-    retrieveAll: retrieveAll(),
-    retrieveByOrganization: retrieveByOrganization(),
-    retrieveByPatient: retrieveByPatient()
+     retrieveAll: function (className){
+        const Class = Parse.Object.extend(className);
+
+        let query = new Parse.Query(Class);
+        
+        query.limit(2000);
+
+        query.find().then((results) => {
+            resolve(results);
+        }, (error) => {
+            reject(error);
+        });
+    },
+
+    retrieveAllByOrganization: function (className, organization){
+        var Class = Parse.Object.extend(className);
+        var q = new Parse.Query(Class);
+        q.limit(2000);
+        q.equalTo("surveyingOrganization", organization);
+        q.find().then((results) =>{
+            resolve(results);
+        }, (error) => {
+            reject(error);
+        });
+    },
+
+    retrieveOneByPatient: function (className,patientID){
+        return new Promise((resolve, reject) => {
+            var Class = Parse.Object.extend(className);
+            var q = new Parse.Query(Class);
+            q.limit(2000);
+            q.get(patientID).then((result) =>{
+                resolve(result);
+            }, (error) => {
+                reject(error);
+            });
+        });
+    }
 
 }
