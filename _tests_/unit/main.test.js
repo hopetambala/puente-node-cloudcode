0104029f9904ee68c3601d7f672428c80a77e61c
Hope Tambala <hopetambala@outlook.com>
2020-04-26 11:27:06 -0400

    chore: fix linting

--- _tests_/unit/main.test.js
+++ _tests_/unit/main.test.js
@@ -1,33 +1,30 @@
 
-'use strict';
-const { cloudFunctions } = require('../run-cloud')
+const { cloudFunctions } = require('../run-cloud');
 
-//hello world
+// hello world
 test('Hello World exists', async () => {
-    // expect.assertions(1);
-    // const data = await cloudFunctions.hello()
-    expect(cloudFunctions.hello()).toBeDefined();
+  // expect.assertions(1);
+  // const data = await cloudFunctions.hello()
+  expect(cloudFunctions.hello()).toBeDefined();
 });
 
 // test to ensure it is defined
 test('Post Exists', async () => {
-    expect(cloudFunctions.postObjectsToClass()).toBeDefined();
+  expect(cloudFunctions.postObjectsToClass()).toBeDefined();
 });
 
 test('Post object - async', async () => {
-    // expect.assertions(1);
-    const post_params = {
-        parseClass: "SurveyData",
-        signature: "Joe",
-        photoFile: "pictureofJoe",
-        localObject: {
-            fname: 'Greetings',
-            lname: 'Bouble'
-        }
-    }
-    return cloudFunctions.postObjectsToClass(post_params).then((result)=>{
-        console.log(result)
-    })
-       
-})
-
+  // expect.assertions(1);
+  const post_params = {
+    parseClass: 'SurveyData',
+    signature: 'Joe',
+    photoFile: 'pictureofJoe',
+    localObject: {
+      fname: 'Greetings',
+      lname: 'Bouble',
+    },
+  };
+  return cloudFunctions.postObjectsToClass(post_params).then((result) => {
+    console.log(result);
+  });
+});
