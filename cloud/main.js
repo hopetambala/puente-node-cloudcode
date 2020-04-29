638782c48d30774b5b1b0bbe6de2402dc9f68050
Joseph McCombs <jamccomb92@gmail.com>
2020-04-28 21:23:42 -0400

    Created sign In, sign Up, sign out, forgot password, and current User
    Migrated auth.ts from ionic data collection to node cloudcode

--- cloud/main.js
+++ cloud/main.js
@@ -5,7 +5,6 @@ const Patient = require('./classes/patient.js')
 const Query = require('./functions/aggregate.js')
 
 
-
 Parse.initialize("ZvGwjA7cemNfr9Qtn6LnwnrcgiM3Xl4N3msObrcg", "dQW12E6wgKWrqdlNCYMCIzLzWomgjCZlLZrrXlki"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
 Parse.serverURL = "https://parseapi.back4app.com/";
 
@@ -579,3 +578,140 @@ Parse.Cloud.define("updateObject", function (request, response) {
   })
 })
 
+
+/********************************************
+SIGN UP
+Receives user attributes and registers user
+Should send notification to admin (To be in=mplemented)
+
+Input Paramaters:
+  firstname - user's first name
+  lastname - user's last name
+  username - selectd username for the user
+  email - email address for the user
+  orginzation - nonprofit user belogns to
+  role - role of user within organization
+********************************************/
+Parse.Cloud.define("signup", function (request, response) {
+  return new Promise((resolve, reject) => {
+    var user = new Parse.User();
+    user.set('firstname', String(request.params.firstname));
+    user.set('lastname', String(request.params.lastname));
+    user.set('username', String(request.params.username));
+    user.set('password', String(request.params.password));
+    user.set('email', String(request.params.email));
+    user.set('orginization', String(request.params.orginization));
+    user.set('role', String(request.params.role));
+    user.set('adminVerified', false);
+
+    user.signUp().then((result) => {
+      console.log('User created successfully with name ' + result.get("username") + ' and email: ' + result.get("email"));
+      response.success(resolve(result));
+    }, (error) => {
+      console.log("Error: " + error.code + " " + error.message);
+      response.error(reject(error));
+    })
+
+
+  })
+})
+
+/********************************************
+SIGN IN
+Signs a user in with given username and password
+Will also check the email to see if that is being used
+
+Input Paramaters:
+  username - selected username for the user
+  password - user password
+********************************************/
+Parse.Cloud.define("signin", function (request, response) {
+  return new Promise((resolve, reject) => {
+    Parse.User.logIn(String(request.params.username), String(request.params.password)).then((result) => {
+      console.log('User logged in successful with username: ' + result.get("username"));
+      response.success(resolve(result));
+    }, (error) => {
+      // If the user inputs their email instead of the username
+      // attempt to get the username 
+      console.log('Trying to use email instead of Username');
+      var userQuery = new Parse.Query(Parse.User);
+
+      userQuery.equalTo('email', request.params.username);
+      userQuery.first().then(function (success) {
+        var username = success.toJSON().username;
+        Parse.User.logIn(username, String(request.params.password)).then((result) => {
+          console.log('User logged in successful with email: ' + result.get("email"));
+          response.success(resolve(result));
+        }, (error) => {
+          console.log('Error: ' + error.code + ' ' + error.message);
+          response.error(reject(error));
+        })
+      }, (error) => {
+        console.log('Error: ' + error.code + ' ' + error.message);
+        response.error(reject(error));
+      })
+    })
+  })
+})
+
+/********************************************
+SIGN OUT
+Attempts to log a user out and display success/reject message
+
+Input Paramaters:
+  none
+********************************************/
+Parse.Cloud.define("signout", function (request, response) {
+  return new Promise((resolve, reject) => {
+    Parse.User.logOut().then((result) => {
+      console.log('User successfully logged out');
+      response.success(resolve(result));
+    }, (error) => {
+      console.log('Error: ' + error.code + ' ' + error.message);
+      response.error(reject(error));
+    })
+  })
+})
+
+/********************************************
+FORGOT PASSWORD
+Recevies a user email address and immediately send out a 
+reset email link to the user.
+
+Input Paramaters:
+  email - user's email associated with the account
+********************************************/
+Parse.Cloud.define("forgotPassword", function (request, response) {
+  return new Promise((resolve, reject) => {
+    Parse.User.requestPasswordReset(String(request.params.email)).then(function () {
+      console.log("Password reset request was sent successfully");
+      response.success(resolve("Success"));
+    }, (error) => {
+      console.log('Error: ' + error.code + " " + error.message);
+      response.error(reject(error));
+    })
+  })
+})
+
+/********************************************
+CURRENT USER
+captures the cached current user object to avoid having to 
+log back in every time
+
+Input Paramaters:
+  none
+********************************************/
+Parse.Cloud.define("currentUser", function () {
+  let u = Parse.User.current();
+
+  if (user) {
+    var user = new User();
+    user.id = u.id;
+    user.name = u.get('username');
+    user.email = u.get('email');
+    user.organization = u.get('organization');
+    user.role = u.get('role');
+    return user;
+  }
+  return null;
+})
