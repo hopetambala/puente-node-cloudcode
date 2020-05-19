9500a69b80027bbf6f44b199d838538183afe205
Joseph McCombs <jamccomb92@gmail.com>
2020-05-19 17:28:29 -0400

    Admin users still cannot adjust others

--- cloud/main.js
+++ cloud/main.js
@@ -4,6 +4,7 @@
 const classes = require('./src/classes');
 const services = require('./src/services');
 
+
 Parse.Cloud.define('hello', (request, response) => new Promise((resolve) => {
   response.success(resolve('Hello world!'));
 }));
@@ -414,30 +415,152 @@ Parse.Cloud.define('signup', (request, response) => new Promise((resolve, reject
   user.set('email', String(request.params.email));
   user.set('organization', String(request.params.organization));
 
+  var userRole = '';
   //Query to count number of users for the organization passed into function
   let userQuery = new Parse.Query(Parse.User);
   userQuery.equalTo("organization", String(request.params.organization));
   userQuery.count().then((results) => {
     // first user signed up, gets admin accesss
     if (results == 0) {
-      user.set('role', 'Admin');
+      user.set('role', 'administrator');
       user.set('adminVerified', true);
+      userRole = 'administrator';
     }
     else {
       // others need approval from admin
-      user.set('role', 'Contributor');
+      user.set('role', 'contributor');
       user.set('adminVerified', false);
+      userRole = 'contributor';
+
     }
     return user;
   }).then((user) => {
     // sign up user
     user.signUp().then((result) => {
       console.log(`User created successfully with name ${result.get('username')} and email: ${result.get('email')}`);
-      // Parse.Cloud.run('createAdmin', {})
-      // Parse.Cloud.run('addAdmin', { 'user': result.objectID })
-      response.success(resolve(result));
+      Parse.Cloud.useMasterKey();
+      var roleQuery = new Parse.Query(Parse.Role);
+      roleQuery.equalTo("name", userRole);
+
+      roleQuery.first().then((role) => {
+        role.getUsers().add(result);
+        role.save();
+        // user.set('role', userRole)
+        response.success(resolve(result, role))
+      }, (error) => {
+        response.error(reject(error));
+      });
     }, (error) => {
       console.log(`Error: ${error.code} ${error.message}`);
+      response.error(reject(error));
+    });
+  });
+}));
+
+
+
+/**********************************************
+ * Basic user organization query
+ * input: organization
+ *********************************************/
+Parse.Cloud.define('organizationUnverified', (request, response) => new Promise((resolve, reject) => {
+  // const User = Parse.Object.extend(Parse.User);
+  let userQuery = new Parse.Query(Parse.User);
+  // find if there is a current user associated with organization
+  userQuery.equalTo("organization", String(request.params.organization));
+  userQuery.equalTo("adminVerified", false);
+  userQuery.find().then((results) => {
+
+    response.success(resolve(results));
+  }, (error) => {
+    response.error(reject(error));
+  });
+}));
+
+Parse.Cloud.define('organizationVerified', (request, response) => new Promise((resolve, reject) => {
+  // const User = Parse.Object.extend(Parse.User);
+  let userQuery = new Parse.Query(Parse.User);
+  // find if there is a current user associated with organization
+  userQuery.equalTo("organization", String(request.params.organization));
+  userQuery.equalTo("adminVerified", true);
+  userQuery.find().then((results) => {
+
+    response.success(resolve(results));
+  }, (error) => {
+    response.error(reject(error));
+  });
+}));
+
+Parse.Cloud.define('queryRoles', (request, response) => new Promise((resolve, reject) => {
+  const Role = Parse.Object.extend('_Role');
+  const queryRole = new Parse.Query(Role)
+
+  queryRole.find().then((result) => {
+    response.success(resolve(result));
+  }, (error) => {
+    response.error(reject(error));
+  })
+}));
+
+
+Parse.Cloud.define('createAdminRole', (request, response) => {
+  const service = services.roles;
+  return service.createAdminRole();
+});
+
+Parse.Cloud.define('createAdministrator', (request, response) => new Promise((resolve, reject) => {
+  const Role = Parse.Object.extend('_Role');
+  const existingAdminRole = new Parse.Query(Role)
+    .equalTo('name', 'administrator')
+  existingAdminRole.first().then((results) => {
+    // If the admin role already exists we have nothing to do here
+    if (results) {
+      console.log("Admin Exists");
+      response.success(resolve(results))
+      // If the admin role does not exist create it and set the ACLs
+    } else {
+      console.log("Admin DNE");
+      const acl = new Parse.ACL();
+      acl.setPublicReadAccess(true);
+      acl.setPublicWriteAccess(false);
+      acl.setRoleWriteAccess('moderator', true);
+      acl.setRoleWriteAccess('contributor', true);
+      acl.setRoleReadAccess('moderator', true);
+      acl.setRoleReadAccess('contributor', true);
+      const adminRole = new Role();
+      adminRole.set('name', 'administrator');
+      adminRole.setACL(acl);
+      adminRole.save({}, { useMasterKey: true }).then((results) => {
+        response.success(resolve(results));
+      }, (error) => {
+        response.error(reject(error));
+      })
+    }
+  }, (error) => {
+    response.error(reject(error));
+  });
+}));
+
+Parse.Cloud.define('addAdmin', (request, response) => new Promise((resolve, reject) => {
+
+  var userQuery = new Parse.Query(Parse.User);
+  userQuery.equalTo("username", request.params.userID);
+
+  userQuery.find().then((user) => {
+    return user;
+  }).then((user) => {
+    var roleQuery = new Parse.Query(Parse.Role);
+    roleQuery.equalTo("name", request.params.roleName);
+
+    roleQuery.first().then((role) => {
+      var relation = role.relation("users");
+      relation.add(user);
+      role.save().then((results) => {
+        response.success(resolve(results));
+      }, (error) => {
+        response.error(reject(error));
+      })
+    }, (error) => {
       response.error(reject(error));
     });
   }, (error) => {
@@ -445,58 +568,102 @@ Parse.Cloud.define('signup', (request, response) => new Promise((resolve, reject
   })
 }));
 
+Parse.Cloud.define('addToRole', (request, response) => new Promise((resolve, reject) => {
+  var userQuery = new Parse.Query(Parse.User);
+  // userQuery.equalTo("username", request.params.userID);
 
-/**********************************************
- * Basic user organization query
- * input: organization
- *********************************************/
-Parse.Cloud.define('organizationJoinRequests', (request, response) => new Promise((resolve, reject) => {
-  // const User = Parse.Object.extend(Parse.User);
-  let userQuery = new Parse.Query(Parse.User);
-  // find if there is a current user associated with organization
-  userQuery.equalTo("organization", String(request.params.organization));
-  userQuery.equalTo("adminVerified", false);
-  userQuery.find().then((results) => {
-    // if (results[0]) {
-    //   return true;
-    // }
-    // else {
-    //   return false;
-    // }
-    response.success(resolve(results));
+  userQuery.get(request.params.userID).then((user) => {
+    user.set('role', String(request.params.roleName));
+    user.set('adminVerified', true)
+    return user;
+  }).then((user) =>
+    user.save()).then((result) => {
+      Parse.Cloud.useMasterKey();
+      var roleQuery = new Parse.Query(Parse.Role);
+      roleQuery.equalTo("name", String(request.params.roleName));
+      roleQuery.first().then((role) => {
+        role.getUsers().add(result);
+        role.save();
+        response.success(resolve(result));
+      }, (error) => {
+        response.error(reject(error));
+      });
+    }, (error) => {
+      response.error(reject(error));
+    })
+}));
+
+// Parse.Cloud.define('queryRole', (request, response) => new Promise((resolve, reject) => {
+//   var roleQuery = new Parse.Query(Parse.Role);
+
+// }))
+
+Parse.Cloud.define('createModerator', (request, response) => new Promise((resolve, reject) => {
+  const Role = Parse.Object.extend('_Role');
+  const existingModeratorrole = new Parse.Query(Role)
+    .equalTo('name', 'moderator')
+  existingModeratorrole.first().then((results) => {
+    // If the admin role already exists we have nothing to do here
+    if (results) {
+      console.log("Moderator Exists");
+      response.success(resolve(results))
+      // If the admin role does not exist create it and set the ACLs
+    } else {
+      console.log("Moderator DNE");
+      const acl = new Parse.ACL();
+      acl.setPublicReadAccess(true);
+      acl.setPublicWriteAccess(false);
+      acl.setRoleWriteAccess('administrator', false);
+      acl.setRoleWriteAccess('contributor', true);
+      acl.setRoleReadAccess('administrator', false);
+      acl.setRoleReadAccess('contributor', true);
+      const adminRole = new Role();
+      adminRole.set('name', 'moderator');
+      adminRole.setACL(acl);
+      adminRole.save({}, { useMasterKey: true }).then((results) => {
+        response.success(resolve(results));
+      }, (error) => {
+        response.error(reject(error));
+      })
+    }
   }, (error) => {
     response.error(reject(error));
   });
 }));
 
-Parse.Cloud.define('verifyUserAndChangeRole', (request, response) => new Promise((resolve, reject) => {
-  let user
-}))
+Parse.Cloud.define('createContributor', (request, response) => new Promise((resolve, reject) => {
+  const Role = Parse.Object.extend('_Role');
+  const existingContributorRole = new Parse.Query(Role)
+    .equalTo('name', 'contributor')
+  existingContributorRole.first().then((results) => {
+    // If the admin role already exists we have nothing to do here
+    if (results) {
+      console.log("Contributor Exists");
+      response.success(resolve(results))
+      // If the admin role does not exist create it and set the ACLs
+    } else {
+      console.log("Moderator DNE");
+      const acl = new Parse.ACL();
+      acl.setPublicReadAccess(true);
+      acl.setPublicWriteAccess(false);
+      acl.setRoleWriteAccess('administrator', false);
+      acl.setRoleWriteAccess('moderator', false);
+      acl.setRoleReadAccess('administrator', false);
+      acl.setRoleReadAccess('moderator', false);
+      const adminRole = new Role();
+      adminRole.set('name', 'contributor');
+      adminRole.setACL(acl);
+      adminRole.save({}, { useMasterKey: true }).then((results) => {
+        response.success(resolve(results));
+      }, (error) => {
+        response.error(reject(error));
+      })
+    }
+  }, (error) => {
+    response.error(reject(error));
+  });
+}));
 
-// Parse.Cloud.define('createAdmin', (request, response) => new Promise((resolve, reject) => {
-//   const Role = Parse.Object.extend('_Role');
-//   const existingAdminRole = new Parse.Query(Role)
-//     .equalTo('name', 'admin')
-//     .first()
-//   //admin already created, nothing to do here
-//   if (existingAdminRole) {
-//     response.success(resolve("Admin Role Already Exists."))
-//   }
-//   else {
-//     const acl = new Parse.ACL();
-//     acl.setPublicReadAccess(true);
-//     acl.setPublicWriteAccess(false);
-//     acl.setRoleWriteAccess(true);
-//     const adminRole = new Role();
-//     adminRole.set('name', 'admin');
-//     adminRole.setACL(acl)
-//     adminRole.save({}, { useMasterKey: true }).then((results) => {
-//       response.success(resolve(results));
-//     }, (error) => {
-//       response.error(reject(error));
-//     })
-//   }
-// }));
 
 // Parse.Cloud.define('addAdmin', (request, response) => new Promise((resolve, reject) => {
 //   user = request.params.user;
--- cloud/src/services/index.js
+++ cloud/src/services/index.js
@@ -1,7 +1,9 @@
 const Batch = require('./batch/batch.js');
+const Roles = require('./roles/roles.js');
 
 const services = {};
 
 services.batch = Batch;
+services.roles = Roles;
 
 module.exports = services;
