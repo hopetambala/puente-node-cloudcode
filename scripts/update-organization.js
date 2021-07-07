4d853a04296966ac3913e00c7b2e94ea2cc6fbbe
Joseph McCombs <jamccomb92@gmail.com>
2021-07-06 21:57:11 -0400

    chore:linting

--- scripts/update-organization.js
+++ scripts/update-organization.js
@@ -1,11 +1,11 @@
 const { Parse } = require('parse/node');
+const prompt = require('prompt');
+const { toUpper } = require('lodash');
 const {
   PARSE_ENV, PARSE_APP_ID, PARSE_JAVASCRIPT_KEY, PARSE_SERVER_URL,
 } = require('./env.config');
-const prompt = require('prompt');
-const { toUpper } = require('lodash');
 
-console.log( PARSE_ENV, PARSE_APP_ID, PARSE_JAVASCRIPT_KEY, PARSE_SERVER_URL)
+console.log(PARSE_ENV, PARSE_APP_ID, PARSE_JAVASCRIPT_KEY, PARSE_SERVER_URL); // eslint-disable-line
 if (PARSE_ENV === 'staging') {
   Parse.initialize(PARSE_APP_ID, PARSE_JAVASCRIPT_KEY);
   Parse.serverURL = PARSE_SERVER_URL;
@@ -14,58 +14,65 @@ if (PARSE_ENV === 'staging') {
   Parse.serverURL = PARSE_SERVER_URL;
 }
 
-prompt.start()
+prompt.start();
 
-function getParameters(keyword) {
-    console.log(`${keyword} the current organization and new organization you wish to update with.`)
-    prompt.get(['currentValue','newValue'], (err, result) => {
-        if (err) { return onErr(err); }
-        checkCorrect(result);
-    })
+function queryAndUpdateSurveyData(params) {
+  const basicQueryParams = {
+    offset: 0,
+    limit: 1000000,
+    parseColumn: 'surveyingOrganization',
+    parseParam: params.currentValue,
+  };
+
+  Parse.Cloud.run('basicQuery', basicQueryParams).then((response) => {
+      console.log(`Total objects queried: ${response.length}`); // eslint-disable-line
+      console.log('If total objects queried is 3000, node-cloud may have maxxed out and the process will need to be reran.'); // eslint-disable-line
+    response.forEach((surveyData) => {
+      const survey = surveyData.toJSON();
+      const postParams = {
+        parseClass: 'SurveyData',
+        parseClassID: survey.objectId,
+        localObject: {
+          surveyingOrganization: params.newValue,
+        },
+      };
+      Parse.Cloud.run('updateObject', postParams).then(() => {
+      }, () => {
+          console.log(`Failed to update ObjectID: ${survey.objectId}`); // eslint-disable-line
+      });
+    });
+  });
 }
 
 function checkCorrect(params) {
-    console.log('Are these the correct versions? (y/n)');
-    for(var key in params) {
-        console.log(`  ${key} ${params[key]}`);
-    }
-    prompt.get(['correct'], (err, result) => {
-      if (err) { return onErr(err); }
-      console.log(`Correct: ${result.correct}`);
-      if (toUpper(result.correct) === 'Y') {
-        queryAndUpdateSurveyData(params);
-      } else {
-        getParameters('Re-enter');
-      }
-    });
-  }
+    console.log('Are these the correct versions? (y/n)'); // eslint-disable-line
+  Object.keys(params).forEach((key) => {
+        console.log(`  ${key}: ${params[key]}`); // eslint-disable-line
+  });
 
-function queryAndUpdateSurveyData (params) {
-    const basicQueryParams = {
-        offset: 0,
-        limit: 1000000,
-        parseColumn: 'surveyingOrganization',
-        parseParam: params.currentValue
+  prompt.get(['correct'], (err, result) => {
+      if (err) { console.log(err); } // eslint-disable-line
+      console.log(`Correct: ${result.correct}`); // eslint-disable-line
+    if (toUpper(result.correct) === 'Y') {
+      queryAndUpdateSurveyData(params);
+    } else {
+      return 0;
     }
-
-    Parse.Cloud.run('basicQuery', basicQueryParams).then((response) => {
-        console.log(`Total objects queried: ${response.length}`);
-        console.log(`If total objects queried is 3000, node-cloud may have maxxed out and the process will need to be reran.`)
-        response.forEach((surveyData) => {
-            const survey = surveyData.toJSON();
-            const postParams = {
-                parseClass: 'SurveyData',
-                parseClassID: survey.objectId,
-                localObject: {
-                    surveyingOrganization: params.newValue
-                }
-            }
-            Parse.Cloud.run('updateObject', postParams).then(() => {
-            }, () => {
-                console.log(`Failed to update ObjectID: ${survey.objectId}`);
-            })
-        });
-    })
+    return -1;
+  });
 }
+
+function getParameters(keyword) {
+  console.log(`${keyword} the current organization and new organization you wish to update with.`); // eslint-disable-line
+  prompt.get(['currentValue', 'newValue'], (err, result) => {
+    if (err) { console.log(err); } // eslint-disable-line
+    const checkedResult = checkCorrect(result);
+    if (checkedResult === 0) {
+      getParameters('Re-enter');
+    }
+    return 0;
+  });
+}
+
 
 getParameters('Enter');
